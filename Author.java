68decf4490fc40f03d96533883393225a69f76ce
MiguelFeranil <152839582+MiguelFeranil@users.noreply.github.com>
2024-04-04 04:15:39 +0000

    HashMap Seatwork

---
+++ Author.java
@@ -0,0 +1,22 @@
+public class Author {
+    private String name;
+    private String bio;
+
+    // getters
+    public String getName() {
+        return name;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    // setters
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+}
---
+++ authorName.java
@@ -0,0 +1,9 @@
+
+public class authorName {
+
+    public static String[] values() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'values'");
+    }
+
+}
